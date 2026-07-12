ccb006e3cf375aadfb478653e8ca14aa75827f44
Trash Robot <lafelabs@gmail.com>
2026-07-11 19:37:47 -0600

    Update fork.php

--- fork.php
+++ fork.php
@@ -1,4 +1,19 @@
-<a href="fork/index.html">fork/index.html</a>
+<?php
+
+$fork = isset($_GET["fork"]) ? $_GET["fork"] : "fork";
+
+if (!is_dir($fork)) {
+    mkdir($fork);
+}
+
+$files = json_decode(file_get_contents('spore.json'), true);
+
+foreach ($files as $file) {
+    copy($file, $fork . "/" . $file);
+}
+
+?>
+<a href="<?php echo $fork?>/index.html"><?php echo $fork?>/index.html</a>
 <style>
 body {
     font-size: 3em;
