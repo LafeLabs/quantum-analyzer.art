34ed209c0a20926a016a7d41a59c87f533933837
Trash Robot <lafelabs@gmail.com>
2026-07-11 18:31:59 -0600

    Update list-directories.php

--- list-directories.php
+++ list-directories.php
@@ -1,1 +1,9 @@
-["control-panel","fork","geometron","spore","warm-rf-network"]
+<?php
+
+    $files = scandir(getcwd());
+    $dirs = array_filter($files, function ($value) {
+        return $value[0] !== '.' && is_dir($value);
+    });
+    echo json_encode(array_values($dirs));
+
+?>
